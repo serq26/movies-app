caa058d4a16a4362c46c002f8c970b8870942fc3
serq26 <serhat26basak@gmail.com>
2022-09-03 12:10:23 +0300

    Theme Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import { ThemeContext } from "./contexts/ThemeContext";
 
 export default function App() {
+  const { mode } = useContext(ThemeContext);
+
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
   return (
-    <div className="app">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Header />
       <Home />
-    </div>
+    </ThemeProvider>
   );
 }
