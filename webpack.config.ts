c7cfa7799a17864481ea101ec7c2efbd8b686fd8
serq26 <serhat26basak@gmail.com>
2022-09-06 16:45:48 +0300

    Webpack Configurations

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,53 +1,57 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
+const { merge } = require("webpack-merge");
+const modeConfiguration = env => require(`./build-utils/webpack.${env}`)(env);
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(css)$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(jpe?g|gif|png|svg)$/,
-        use: "url-loader",
-      },
+module.exports = ({ mode } = { mode: "production" }) => {
+  return merge({
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "main.bundle.js",
+    },
+    mode,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(css)$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(jpe?g|gif|png|svg)$/,
+          use: "url-loader",
+        },
+      ],
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new Dotenv(),
+      new HtmlWebpackPlugin({
+        title: "MovieS App",
+        minify: true,
+        template: "public/index.html",
+      }),
     ],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),  
-    new Dotenv(),
-    new HtmlWebpackPlugin({
-      title: "MovieS App",
-      minify: true,
-      template: "public/index.html",
-    }),
-  ],
-  devServer: {
-    compress: true,
-    port: 3000,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: [".js", ".tsx", ".ts"],
-  },
+    devServer: {
+      compress: true,
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+    resolve: {
+      extensions: [".js", ".tsx", ".ts"],
+    },
+  });
 };
